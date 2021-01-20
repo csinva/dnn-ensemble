db44956221d02dea8a60b4da5ed30dc92a0ebf32
Chandan Singh <chandan_singh@berkeley.edu>
2021-01-20 16:56:23 -0500

    remove extraneous files

--- pcs/__init__.py
+++
@@ -1,6 +0,0 @@
-"""
-.. include:: ../readme.md
-"""
-from .perturbation import *
-from .perturbation_set import *
-from .pipeline import *
--- pcs/perturbation_set.py
+++
@@ -1,7 +0,0 @@
-'''Set of perturbations to be used in pipeline
-'''
-
-
-class PerturbationSet():
-    def __init__(self):
-        self.perturbations = []
